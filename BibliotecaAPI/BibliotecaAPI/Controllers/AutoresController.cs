69b9d661ada80b1de360905e8a8d22734922578b
ESaulDJLaguna <esdjl_asse15@hotmail.com>
2026-06-20 15:33:47 -0600

    Clean project: delete controller, middlewares, repositories, examples and unnecessary actions

--- BibliotecaAPI/BibliotecaAPI/Controllers/AutoresController.cs
+++ BibliotecaAPI/BibliotecaAPI/Controllers/AutoresController.cs
@@ -10,36 +10,23 @@ namespace BibliotecaAPI.Controllers
     public class AutoresController : ControllerBase // ControllerBase es una clase base que tiene funcionalidad auxiliar que permite trabajar de una manera sencilla con un web API, por ejemplo, tiene métodos auxiliares para devolver respuestas HTTP.
     {
         private readonly ApplicationDbContext _context;
-        private readonly ILogger<AutoresController> logger;
 
-        public AutoresController(ApplicationDbContext context, ILogger<AutoresController> logger)
+        public AutoresController(ApplicationDbContext context)
         {
             _context = context;
-            this.logger = logger;
         }
 
-        // Es posible definir que una acción responda a varias rutas. En este ejemplo utilizamos [HttpGet("/listado-de-autores")] y [HttpGet]
-        [HttpGet("/listado-de-autores")] // Es posible ignorar la ruta (api/autores) y darle una ruta personalizada a una acción. Para eso el nombre en la plantilla debe iniciar por /. Por lo tanto, la ruta para esta acción sería: https://midominio.com/listado-de-autores
         [HttpGet]
         public async Task<IEnumerable<Autor>> Get()
         {
-            logger.LogTrace("Obteniendo el listado de autores - Trace");
-            logger.LogDebug("Obteniendo el listado de autores - Debug");
-            logger.LogInformation("Obteniendo el listado de autores - Information");
-            logger.LogWarning("Obteniendo el listado de autores - Warning");
-            logger.LogError("Obteniendo el listado de autores - Error");
-            logger.LogCritical("Obteniendo el listado de autores - Critical");
             return await _context.Autores.ToListAsync();
         }
 
-        [HttpGet("primero")] // api/autores/primero
-        public async Task<Autor> GetPrimerAutor() => await _context.Autores.FirstAsync();
-
         // {id:int} es una plantilla de ruta, lo que significa que el valor que se envíe en esa parte de la URL va a ser interpretado como un entero y va a ser asignado al parámetro 'id' del método Get(int id). Hay que tener en cuenta que no lleva espacio entre id e int, sino que es id:int
-        [HttpGet("{id:int}")] // api/autores/id?incluirLibros=true|false
+        [HttpGet("{id:int}")] // api/autores/id
         // ActionResult: es un tipo de retorno que representa el resultado de una acción en un controlador. Permite devolver diferentes tipos de respuestas HTTP, como Ok(), NotFound(), BadRequest(), etc., dependiendo del resultado de la operación realizada en el método. Permite devolver cualuier tipo de código de estatus HTTP (404, 500, etc.) o un Autor.
         //public async Task<ActionResult<Autor>> Get([FromRoute] int id, [FromQuery] bool incluirLibros) // incluirLibros se envía a través de un query string
-        public async Task<ActionResult<Autor>> Get([FromRoute] int id, [FromHeader] bool incluirLibros)
+        public async Task<ActionResult<Autor>> Get([FromRoute] int id)
         {
             Autor? autor = await _context.Autores.Include(x => x.Libros).FirstOrDefaultAsync(x => x.Id == id);
 
@@ -47,23 +34,6 @@ namespace BibliotecaAPI.Controllers
 
             return autor;
         }
-
-        /*
-         * Los parámetros de ruta nos permiten definir variables a nivel de la URL. Esto nos permite mayor libertad a la hora de configurar las rutas de nuestras acciones.
-         * El tipo de parámetro string no existe para los tipos de variable de ruta, para eso se utiliza 'alpha'. Si no se le coloca un tipo de dato (al cual se le conoce como restricción de variable de ruta), podremos utilizar un string cualquiera (es decir, se aceptan símbolos, números, letras, etc.)
-         */
-        [HttpGet("{nombre:alpha}")]
-        public async Task<IEnumerable<Autor>> Get(string nombre) => await _context.Autores.Where(x => x.Nombre.Contains(nombre)).ToListAsync();
-
-        // Una acción puede tener más de un parámetro de ruta y también se puede indicar que los últimos son opcionales (utilizando el símbolo ?), en caso de ser necesario, se les puede dar un valor por defecto (para ello el símbolo ? en "string? parametro2" ya no es necesario)
-        //[HttpGet("{parametro1}/{parametro2}")] // api/autores/valor1/valor2 => parametro2 es obligatorio
-        //[HttpGet("{parametro1}/{parametro2?}")] // api/autores/valor1/valor2 => parametro2 es opcional, si no se envía en la URL el valor de parametro2 es null
-        //public ActionResult Get(string parametro1, string? parametro2) // Parámetro opcional (si no se envia en la URL el valor es null)
-        //public ActionResult Get(string parametro1, string parametro2 = "Valor por defecto") // Parámetro con valor por defecto
-        //{
-        //return Ok(new { parametro1, parametro2 });
-        //}
-
 
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] Autor autor)
--- BibliotecaAPI/BibliotecaAPI/Entidades/Autor.cs
+++ BibliotecaAPI/BibliotecaAPI/Entidades/Autor.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BibliotecaAPI.Entidades
 {
-    public class Autor// : IValidatableObject
+    public class Autor
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")] // El atributo [Required] indica que el campo es obligatorio (es una validación de ASP.NET Core)
@@ -12,39 +12,5 @@ namespace BibliotecaAPI.Entidades
         public required string Nombre { get; set; } // El modificador 'required' es una característica de C# 11 que indica que esta propiedad debe ser inicializada al crear una instancia de la clase. Esto es útil para garantizar que se proporcionen valores para propiedades esenciales, como 'Nombre' en este caso.
 
         public List<Libro> Libros { get; set; } = new List<Libro>();
-
-        /*! Ejemplo con múltiples errores de validación por modelo
-        public int Edad { get; set; }
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(!string.IsNullOrEmpty(Nombre))
-            {
-                var primeraLetra = Nombre[0].ToString();
-                if (primeraLetra != primeraLetra.ToUpper())
-                {
-                    yield return new ValidationResult("La primera letra debe ser mayúscula - por modelo", new string[] { nameof(Nombre) });
-                }
-            }
-
-            if(Nombre.Length < 5)
-            {
-                yield return new ValidationResult($"El {nameof(Nombre)} no debe tener más de 5 caracteres", new string[] { nameof(Nombre) });
-            }
-
-            if(Edad < 18)
-            {
-                yield return new ValidationResult("Debe ser mayor de edad", new[] { nameof(Edad) });
-            }
-        }
-        */
-
-        /* Otras validaciones por defecto
-        [Range(18, 120, ErrorMessage = "La {0} debe ser mayor o igual que {1} pero menor o igual a {2}")]
-        public int Edad { get; set; }
-        [CreditCard] // Solo valida el formato de la tarjeta de crédito
-        public string? TarjetaDeCredito { get; set; }
-        [Url]
-        public string? URL { get; set; }
-        */
     }
 }
